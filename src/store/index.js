17daed2ce961ff246e42e0433b8e7af8b89cf111
Andrii Butenko <abute@softserveinc.com>
2023-08-29 09:42:26 +0300

    UC#8 React initial setup

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+import { configureStore  } from '@reduxjs/toolkit';
+
+const ADD_DATA = 'ADD_DATA';
+
+export const preloadedState = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: ''
+};
+
+const reducer = (state = preloadedState, action) => {
+    switch(action.type){
+        case ADD_DATA:
+            return action.data;
+    default:
+        return state;
+    }
+};
+
+export const updateForm = (data) => {
+    return {
+        type: ADD_DATA,
+        data,
+    }
+};
+
+export const store = configureStore({
+    reducer,
+    preloadedState,
+});
